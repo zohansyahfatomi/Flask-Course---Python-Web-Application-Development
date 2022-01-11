4535473a9c9f0effa656813e3e5ca4074972d2d0
zohansyahfatomi <zohansyahfatomi@gmail.com>
2022-01-11 19:04:03 +0700

    flask_sqlalchemy can't run in python2.7

--- market copy.py
+++ market copy.py
@@ -1,16 +1,13 @@
-#from enum import unique
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-from werkzeug.exceptions import LengthRequired
-
-app = Flask(__name__) #built-in variable 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30),nullable=False, unique=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
@@ -18,7 +15,7 @@ class Item(db.Model):
     def __repr__(self):
         return f'Item {self.name}'
 
-@app.route('/') #decorators
+@app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
@@ -26,14 +23,4 @@ def home_page():
 @app.route('/market')
 def market_page():
     items = Item.query.all()
-
-    '''
-    items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
-    return render_template('market.html',items = items)
-    '''
-    
-
+    return render_template('market.html', items=items)
