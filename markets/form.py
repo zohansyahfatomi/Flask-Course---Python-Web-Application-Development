7781ffdd09006ff3b6f318cb0b96c975d0a3a6bb
zohansyahfatomi <zohansyahfatomi@gmail.com>
2022-01-12 12:38:01 +0700

    p10. advanced validations

--- markets/form.py
+++ markets/form.py
@@ -1,10 +1,23 @@
 from werkzeug.utils import validate_arguments
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from markets.models import User
 
 #validation form
 class RegisterForm(FlaskForm):
+
+    #username is unique
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username is alraedy exist! Please try different username')
+    
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address = email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address is already exist! Please try different email address')
+
     username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
